8ab099a426716df53bfcd53275831b76ca66881d
Geet Patel <geetpofficial@gmail.com>
2024-06-30 11:36:35 -0400

    Making tab width 4

--- src/handler.c
+++ src/handler.c
@@ -2,25 +2,25 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-  if (!strcmp(argv[1], "get")) {
-    if (argc == 3) {
-      printf("%s %s\n", argv[1], argv[2]);
+    if (!strcmp(argv[1], "get")) {
+        if (argc == 3) {
+            printf("%s %s\n", argv[1], argv[2]);
+        } else {
+            printf("ERROR: Invalid number of arguments\n");
+        }
+    } else if (!strcmp(argv[1], "set")) {
+        if (argc == 4) {
+            printf("%s %s %s\n", argv[1], argv[2], argv[3]);
+        } else {
+            printf("ERROR: Invalid number of arguments\n");
+        }
+    } else if (!strcmp(argv[1], "del")) {
+        if (argc == 4) {
+            printf("%s %s %s\n", argv[1], argv[2], argv[3]);
+        } else {
+            printf("ERROR: Invalid number of arguments\n");
+        }
     } else {
-      printf("ERROR: Invalid number of arguments\n");
+        printf("ERROR: Unrecognised command\n");
     }
-  } else if (!strcmp(argv[1], "set")) {
-    if (argc == 4) {
-      printf("%s %s %s\n", argv[1], argv[2], argv[3]);
-    } else {
-      printf("ERROR: Invalid number of arguments\n");
-    }
-  } else if (!strcmp(argv[1], "del")) {
-    if (argc == 4) {
-      printf("%s %s %s\n", argv[1], argv[2], argv[3]);
-    } else {
-      printf("ERROR: Invalid number of arguments\n");
-    }
-  } else {
-    printf("ERROR: Unrecognised command\n");
-  }
 }
